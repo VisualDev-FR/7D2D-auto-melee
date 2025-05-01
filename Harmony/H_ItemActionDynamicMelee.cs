969043e2959cda97e7d9651bbd3dd4f7b4f5bcc1
Thomas Menanteau <91687641+VisualDev-FR@users.noreply.github.com>
2025-05-01 13:26:28 +0200

    code cleaning

--- Harmony/H_ItemActionDynamicMelee.cs
+++ Harmony/H_ItemActionDynamicMelee.cs
@@ -1,7 +1,4 @@
-using System.Collections.Generic;
-using System.Linq;
 using HarmonyLib;
-using Platform;
 using UnityEngine;
 using ItemActionDynamicMeleeData = ItemActionDynamicMelee.ItemActionDynamicMeleeData;
 
@@ -9,47 +6,27 @@ using ItemActionDynamicMeleeData = ItemActionDynamicMelee.ItemActionDynamicMelee
 [HarmonyPatch(typeof(ItemActionDynamicMelee), "OnHoldingUpdate")]
 public class H_ItemActionDynamicMelee_OnHoldingUpdate
 {
-    public static readonly HashSet<int> actionDatas = new HashSet<int>();
+    public static bool actionRunning = false;
 
-    public static readonly Dictionary<int, float> useTimes = new Dictionary<int, float>();
+    public static float lastUseTime = 0;
 
     private static bool MiddleMousePressed(ItemActionData actionData)
     {
         if (!Input.GetMouseButton(2))
             return false;
 
-        var entityID = actionData.invData.holdingEntity.entityId;
-
-        if (!useTimes.ContainsKey(entityID))
-            useTimes[entityID] = 0;
-
-        if (Time.time - useTimes[entityID] > 0.5f)
+        if (Time.time - lastUseTime > 0.5f)
         {
-            useTimes[entityID] = Time.time;
+            lastUseTime = Time.time;
             return true;
         }
 
         return false;
     }
 
-    private static void AddAction(ItemActionData actionData)
-    {
-        actionDatas.Add(actionData.invData.holdingEntity.entityId);
-    }
-
-    private static void RemoveAction(ItemActionData actionData)
-    {
-        actionDatas.Remove(actionData.invData.holdingEntity.entityId);
-    }
-
-    public static bool ActionExists(ItemActionData actionData)
-    {
-        return actionDatas.Contains(actionData.invData.holdingEntity.entityId);
-    }
-
     private static void ForceExecuteAction(ItemActionData actionData)
     {
-        actionData.invData.holdingEntity.inventory.Execute(0, false, PlatformManager.NativePlatform.Input.PrimaryPlayer);
+        actionData.invData.holdingEntity.inventory.Execute(0, false, null);
     }
 
     public static void Postfix(ItemActionDynamicMelee __instance, ItemActionData _actionData)
@@ -58,27 +35,20 @@ public class H_ItemActionDynamicMelee_OnHoldingUpdate
             return;
 
         var actionDataMelee = _actionData as ItemActionDynamicMeleeData;
-
         bool middleMousePressed = MiddleMousePressed(_actionData);
-        bool actionExists = ActionExists(_actionData);
 
-        if (middleMousePressed && !actionExists && !actionDataMelee.Attacking)
+        if (middleMousePressed && !actionRunning && !actionDataMelee.Attacking)
         {
-            AddAction(_actionData);
-            Log.Out($"[OnHoldingUpdate] add");
+            actionRunning = true;
         }
-        else if (middleMousePressed && actionExists)
+        else if (middleMousePressed && actionRunning)
         {
-            RemoveAction(_actionData);
-            Log.Out($"[OnHoldingUpdate] remove");
+            actionRunning = false;
         }
 
-        if (ActionExists(_actionData) && __instance.canStartAttack(actionDataMelee))
+        if (actionRunning && __instance.canStartAttack(actionDataMelee))
         {
             ForceExecuteAction(_actionData);
-
-            var values = string.Join(", ", actionDatas.Select(a => a.ToString()));
-            Log.Out($"[OnHoldingUpdate] execute action: [{values}]");
         }
     }
 
